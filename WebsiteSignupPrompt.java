1dbff1624a0d41f2e7481746a893ae70ec0df3e6
Hamza <hmmza.py@gmail.com>
2025-05-09 23:38:42 +0800

    WebsiteSignupPrompt.java
    📩 A basic Java CLI simulation of a website sign-up or login prompt. The program asks the user if they already have an account and responds accordingly. It uses simple input handling and conditionals to mimic basic user interaction for account access or registration.
    
    💡 Great for beginners learning how to use Scanner and if-else logic in Java!

---
+++ WebsiteSignupPrompt.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class websitesignup{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi do you have account or you want to signup?");
+        System.out.println("(type Y(If you have account)or N(if you don't have account))");
+        char decision = scanner.next().charAt(0);
+        if (decision == 'Y'){
+            System.out.println("Sure type in your mail");
+            String mail = scanner.next();
+        }else if (decision == 'N'){
+            System.out.println("Okay let's make your account first (:)");
+        }else{
+            System.out.println("Sorry typein valid command");
+        }
+        scanner.close();
+    }
+
+}
